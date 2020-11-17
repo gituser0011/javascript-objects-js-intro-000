520df35743ced46f039416666380dd8e2fd195c0
S  1 <lokohopo@gmail.com>
2020-11-17 00:35:26 +0000

    Automatically backed up by Learn

---
+++ objects.js
@@ -0,0 +1,12 @@
+var playlist = {
+  artistTitle: "songTitle"
+}
+
+function updatePlaylist(playlist, artistName, songTitle){
+playlist [artistName] = songTitle
+return playlist
+}
+
+function removeFromPlaylist(playlist, artistName){
+  return delete playlist.artistName;
+}
